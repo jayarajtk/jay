5d4ad2a673a270d22c1929fc89f0cf03f419772b
jayarajtk <jayarajtk3@gmail.com>
2017-10-09 14:56:08 -0500

    RequestMapping in method level

--- springbootkid/src/main/java/com/springbootkid/trails/starter/helloworld/HelloController.java
+++ springbootkid/src/main/java/com/springbootkid/trails/starter/helloworld/HelloController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by jt316g on 10/7/2017.
  */
 @RestController
-@RequestMapping(value = "/")
 public class HelloController {
 
+    @RequestMapping(value = "/")
     public String sayHelloWorld() {
         return "Hello World!";
     }
